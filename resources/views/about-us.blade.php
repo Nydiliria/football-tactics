72dd46e902541d82c64941cf158b337c38d6355b
Nydiliria <Nydiliriayt@gmail.com>
2025-10-08 12:55:11 +0200

    practiced with routes, views and controller

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,8 @@
+<h1>About {{$company}}</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores atque culpa est eveniet exercitationem
+    ipsa nam nostrum officia perferendis placeat quasi quos ratione repellendus sed, sunt suscipit tenetur
+    voluptatum?</p>
+
+<a href="{{ route('about', ['company' => 'Nintendo']) }}">About Nintendo</a>
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about-us/{company}', function (string $company) {
+    return view('about-us', [
+        'company' => $company
+    ]);
+})->name('about');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('products', ProductController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +26,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
